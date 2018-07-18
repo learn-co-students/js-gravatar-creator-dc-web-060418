4bc628104761292bd2ad8ef63ae0e73d6eab5118
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-18 19:36:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-function handleSubmit(e) {
+function clearAllSquares() {
+  for (let cols = 0; cols < 5; cols++) {
+    for (let rows = 0; rows < 5; rows++) {
+      document.getElementById(`${rows}-${cols}`).style.backgroundColor = ""
+    }
+  }
+}
+
+function handleSubmit(event) {
+  clearAllSquares();
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  console.log(name);
+  const myIdenticon = new Identicon(name);
+  myIdenticon.iterateAndColorEvenSquares();
+  myIdenticon.colorMirrored();
 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
+
 })
