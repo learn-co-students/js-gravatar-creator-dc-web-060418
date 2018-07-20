b41f03084ac86b4f746248a0b4de6fbd9db810eb
github username <github email address>
2018-07-19 21:56:17 -0400

    Done.

--- src/identicon.js
+++ src/identicon.js
@@ -1,3 +1,37 @@
 class Identicon {
-  // make use of the md5.array method, which is in scope because we required it in our index.html
+
+  constructor(string) {
+    let md5Array = md5.array(string)
+    let color = `rgb(${md5Array[0]}, ${md5Array[1]}, ${md5Array[2]})`
+
+    this.md5Array = md5Array
+    this.color = color
+    console.log(color)
+}
+
+  makeBetterAssociationObject() {
+
+    let nestedArray = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
+    let gridAssociation = {}
+    let i = 0
+    for ( const nest1 in nestedArray ) {
+      for ( const nest2 in nestedArray ) {
+        if (parseInt(nest2) == 3) {
+          i -= 2
+          gridAssociation[`${nest1}-${nest2}`] = this.md5Array[i]
+        } else if (parseInt(nest2) == 4) {
+          gridAssociation[`${nest1}-${nest2}`] = this.md5Array[--i]
+        } else {
+          gridAssociation[`${nest1}-${nest2}`] = this.md5Array[i++]
+        }
+
+        if (parseInt(nest2) == 4) {
+          i+=3
+        }
+
+      }
+    }
+    return gridAssociation
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-function handleSubmit(e) {
-
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
 })
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let input = document.querySelector("input")
+  let identicon = new Identicon(input.value)
+  identiconGrid(identicon.makeBetterAssociationObject(), identicon.color)
+}
+
+function findGridIds() {
+  return Array.from(document.querySelector("#identicon").children).map( el => el.id)
+}
+
+function identiconGrid(object, color) {
+  for(const key in object) {
+    document.getElementById(key).style.backgroundColor = ""
+    if (object[key] % 2 === 0) {
+      document.getElementById(key).style.backgroundColor = color
+    }
+  }
+
+}
